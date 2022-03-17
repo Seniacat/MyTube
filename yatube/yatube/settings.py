cb334a46688c0f60150a4df246cf9d7cdef51c20
Seniacat <senya.sidorovich@gmail.com>
2022-03-17 21:53:26 +0300

    settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres1'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='Ksu090787'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT')
     }

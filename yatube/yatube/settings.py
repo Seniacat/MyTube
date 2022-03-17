27d178ab21475ca2a0b3715edcfc9389f08554bc
Seniacat <senya.sidorovich@gmail.com>
2022-03-17 21:45:38 +0300

    settings again

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'yatube.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
+        'NAME': os.getenv('DB_NAME', default='postgres1'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
